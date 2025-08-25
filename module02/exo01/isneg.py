f9d0d6d0e6502d0799776758e6e07a049417100c
Cherica <riantsoacherica01@gmail.com>
2025-08-25 08:51:30 +0300

    androany

---
+++ module02/exo01/isneg.py
@@ -0,0 +1,7 @@
+nb =float( input(":"))
+if nb == 0 :
+ print("Thiss number is both positive and negative")
+elif nb < 0 :
+ print("This number is  negqtive ")
+else :
+ print("This number is positive")
